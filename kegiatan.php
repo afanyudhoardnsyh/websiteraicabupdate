7b43f27466fade27da91a51719e9fde9e179692e
afanyudhoardiansyah <121146698+afanyudhoardnsyh@users.noreply.github.com>
2024-03-31 23:12:31 +0700

    Add files via upload

---
+++ kegiatan.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Raicab | Kegiatan</title>
+    <link rel="stylesheet" href="css/stylegiat.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- Google Fonts Links For Icon -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+</head>
+<body>
+<header>
+    <nav class="navbar">
+    <img src="image/OFC_WHITE Logo Raicab.webp" alt="Raimuna Cabang III Kota Depok">
+        <div class="logo" href="#">RAIMUNA CABANG III KOTA DEPOK</div>
+        <ul class="menu-links">
+        <span id="close-menu-btn" class="material-symbols-outlined">close</span>
+            <li><a href="home.php">BERANDA</a></li>
+            <li><a href="tentang.php">TENTANG</a></li>
+            <li><a href="mediaunduh.php">MEDIA UNDUH</a></li>
+            <li><a href="kegiatan.php" class="active">KEGIATAN</a></li>
+            <div><a href="./login/login.php" class="action_btn">MASUK</a></div>
+        </ul>
+        <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
+    </nav>
+</header>
+
+<!-- kegiatan section start -->
+<section class="kegiatan" id="kegiatan">
+<div class="max-width">
+<h2 class="title">Kegiatan Raimuna Cabang III Kota Depok</h2>
+    <div class="wrapper">
+        <i id="left" class="fa-solid fa-angle-left"></i>
+        <div class="carousel">
+            <img src="image/GiatSako.webp" alt="img" draggable="false">
+            <img src="image/giatrutin.webp" alt="img" draggable="false">
+            <img src="image/GiatWawasan.webp" alt="img" draggable="false">
+            <img src="image/GiatPinkon.webp" alt="img" draggable="false">
+            <img src="image/GiatFunsport.webp" alt="img" draggable="false">
+            <img src="image/GiatUmum.webp" alt="img" draggable="false">
+            <img src="image/GiatPrestasi.webp" alt="img" draggable="false">
+            <img src="image/GiatOutdoor.webp" alt="img" draggable="false">
+            <img src="image/GiatWisata.webp" alt="img" draggable="false">
+            <img src="image/GiatBudaya.webp" alt="img" draggable="false">
+        </div>
+        <i id="right" class="fa-solid fa-angle-right"></i>
+    </div>
+</div>
+</section>
+
+<!-- contact section start -->
+<section class="contact" id="contact">
+        <div class="max-width">
+            <div class="contact-content">
+                <div class="column left">
+                    <img src="image/OFC_Logo_Raicab_Polos.webp">
+                    <div class="text-1">RAIMUNA CABANG III</div>
+                    <div class="text-2">KOTA DEPOK</div>
+                    <div class="text-3">2024</div>
+                    <P> Ini adalah situs resmi Raimuna Cabang lll Tahun 2024. Konten yang tercantum di situs ini dimaksudkan untuk tujuan informasional, 
+                        bukan tujuan komersial. Setiap penjualan yang ditampilkan dimaksudkan sebagai tanda kemitraan dan selalu akan mengarahkan Anda 
+                        ke situs mitra kami.
+                    </P>
+                </div>
+                <div class="column right">
+                    <div class="text">Kontak Kami</div>
+                    <div class="alamat">
+                        <div class="text1">Alamat</div>
+                        <div class="text2">Jl. Boulevard Raya Grand Depok City Kalimulya, Kec. Cilodong, Kota Depok, Jawa Barat 16413</div>
+                    <div class="row">
+                        <div class="info-sosmed">
+                            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=afanyudhoardiansyah@gmail.com&su=Raicab%20III%202024&body=Halo Selamat Malam" target="_blank"><img class="email" style="margin:2px" src="image/gmail.webp" width="30" height="30" alt="Email"></a>
+                            <a href="https://www.instagram.com/dkc_kotadepok" target="_blank"><img  class="ig" style="margin:5px" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" width="25" height="25" alt="Instagram"></a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="container">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7355482140233!2d106.82496667423598!3d-6.428011162858869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebf8cc5cf5bd%3A0x1cb0348aaa9190e1!2sGedung%20Pramuka%20Depok%20(Kwarcab%20Depok)!5e0!3m2!1sid!2sid!4v1709527268342!5m2!1sid!2sid" width="200" height="145" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<!-- footer section start -->
+<footer>
+        <div class="row">
+            <div class="info-footer">
+                <a href="#" target="_blank"><img class="maskot" src="image/Maskot-Raicab.webp" alt="Maskot Raicab"></a>
+                <a href="#" target="_blank"><img class="logo" src="image/OFC_Logo_Raicab_Polos.webp" alt="Logo Raicab"></a>
+            </div>
+            <div class="text">Raimuna Cabang III</div>
+            <div class="text1">|</div> 
+            <div class="text2">2024 All rights reserved.</div> 
+        </div>
+</footer>
+
+    <script src="asset/app.js"></script>
+    <script src="asset/navbar.js"></script>
+    <script src="asset/activenavbar.js"></script>
+</body>
+</html>
---
+++ tentang.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raicab | Tentang Raicab</title>
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="css/styletentang.css">
+    <!-- Google Fonts Links For Icon -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+</head>
+<body>
+    <header>
+    <nav class="navbar">
+    <img src="image/OFC_WHITE Logo Raicab.webp" alt="Raimuna Cabang III Kota Depok">
+        <div class="logo">RAIMUNA CABANG III KOTA DEPOK</div>
+        <ul class="menu-links">
+        <span id="close-menu-btn" class="material-symbols-outlined">close</span>
+            <li><a href="home.php">BERANDA</a></li>
+            <li><a href="tentang.php" class="active">TENTANG</a></li>
+            <li><a href="mediaunduh.php">MEDIA UNDUH</a></li>
+            <li><a href="kegiatan.php">KEGIATAN</a></li>
+            <div><a href="./login/login.php" class="action_btn">MASUK</a></div>
+        </ul>
+        <!-- <a href="#" class="action_btn">MASUK</a> -->
+        <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
+    </nav>
+    </header>
+
+    <!-- Beranda section start -->
+    <section class="home" id="beranda">
+        <div class="max-width">
+            <div class="home-content">
+                <img src="image/OFC_Logo_Raicab.webp">
+                <div class="text-1">RAIMUNA CABANG III</div>
+                <div class="text-2">KOTA DEPOK</div>
+                <div class="text-3">2024</div>
+                <p>
+                    Raimuna adalah pertemuan Pramuka berbentuk perkemahan yang diselenggarakan untuk Pramuka Penegak dan Pramuka Pandega
+                    baik putera maupun puteri dari berbagai satuan Pramuka. Tujuan Raimuna adalah membina dan mengembangkan persaudaraan dan persatuan 
+                    di kalangan para Pramuka Penegak dan Pramuka Pandega serta memberikan kepada mereka kegiatan kreatif, rekreatif dan produktif yang 
+                    bersifat edukatif. Raimuna Cabang III kota Depok Akan dihadiri seluruh penegak dan pandega yang ada di 11 kwartir ranting. 
+                    Raimuna Cabang III kota Depok tahun 2024 dirancang dan dikemas dalam bentuk kegiatan perkemahan dengan berbagai aktivitas yang 
+                    bersifat aktif, kreatif, produktif, inovatif, dan rekreatif.
+                </p>
+            </div>
+        </div>  
+    </section>
+
+    <!-- Kilas Balik Raicab Section -->
+    <section class="main swiper mySwiper">
+    <div class="judul">
+        <div class="text1"><hr>KILAS BALIK RAIMUNA CABANG II KOTA DEPOK</div>
+        <div class="text2">Kegiatan Raimuna Cabang II dilaksanakan pada 17-21 Desember 2018 di Bumi Perkemahan dan graha wisata Cibubur.</div>
+    </div>
+    <div class="wrapper swiper-wrapper">
+        <div class="slide swiper-slide">
+            <img src="image/datang1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Kedatangan Peserta</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/datang2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Kedatangan Peserta</span>
+        </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/buka1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Pembukaan Raimuna Cabang II</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/buka2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Pembukaan Raimuna Cabang II</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/jumpa1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Jumpa Tokoh</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/jumpa2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Jumpa Tokoh</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/jumpa3.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Jumpa Tokoh</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/jumpa4.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Jumpa Tokoh</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/pentas1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Pentas Seni</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/pentas2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Pentas Seni</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/pentas3.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Pentas Seni</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/spesial1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Spesial</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/spesial2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Spesial</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/lifeskill1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Life Skill</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/lifeskill2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Life Skill</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/wisata1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Wisata</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/wisata2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Wisata</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/wisata3.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Wisata</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/ekshibisi1.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Ajang Ekshibisi</span>
+            </div>
+        </div>
+        <div class="slide swiper-slide">
+            <img src="image/ekshibisi2.jpg" alt="" class="image" />
+            <div class="image-data">
+                <span class="text">Giat Ajang Ekshibisi</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="swiper-button-next nav-btn"></div>
+    <div class="swiper-button-prev nav-btn"></div>
+    <div class="swiper-pagination"></div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <div class="contact-content">
+                <div class="column left">
+                    <img src="image/OFC_Logo_Raicab_Polos.webp">
+                    <div class="text-1">RAIMUNA CABANG III</div>
+                    <div class="text-2">KOTA DEPOK</div>
+                    <div class="text-3">2024</div>
+                    <P> Ini adalah situs resmi Raimuna Cabang lll Tahun 2024. Konten yang tercantum di situs ini dimaksudkan untuk tujuan informasional, 
+                        bukan tujuan komersial. Setiap penjualan yang ditampilkan dimaksudkan sebagai tanda kemitraan dan selalu akan mengarahkan Anda 
+                        ke situs mitra kami.
+                    </P>
+                </div>
+                <div class="column right">
+                    <div class="text">Kontak Kami</div>
+                    <div class="alamat">
+                        <div class="text1">Alamat</div>
+                        <div class="text2">Jl. Boulevard Raya Grand Depok City Kalimulya, Kec. Cilodong, Kota Depok, Jawa Barat 16413</div>
+                    <div class="row">
+                        <div class="info-sosmed">
+                            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=afanyudhoardiansyah@gmail.com&su=Raicab%20III%202024&body=Halo Selamat Malam" target="_blank"><img class="email" style="margin:2px" src="image/gmail.webp" width="30" height="30" alt="Email"></a>
+                            <a href="https://www.instagram.com/dkc_kotadepok" target="_blank"><img  class="ig" style="margin:5px" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" width="25" height="25" alt="Instagram"></a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="container">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7355482140233!2d106.82496667423598!3d-6.428011162858869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebf8cc5cf5bd%3A0x1cb0348aaa9190e1!2sGedung%20Pramuka%20Depok%20(Kwarcab%20Depok)!5e0!3m2!1sid!2sid!4v1709527268342!5m2!1sid!2sid" width="200" height="145" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <div class="row">
+            <div class="info-footer">
+                <a href="#" target="_blank"><img class="maskot" src="image/Maskot-Raicab.webp" alt="Maskot Raicab"></a>
+                <a href="#" target="_blank"><img class="logo" src="image/OFC_Logo_Raicab_Polos.webp" alt="Logo Raicab"></a>
+            </div>
+            <div class="text">Raimuna Cabang III</div>
+            <div class="text1">|</div> 
+            <div class="text2">2024 All rights reserved.</div> 
+        </div>
+    </footer>
+
+        
+    <script src="asset/navbar.js"></script>
+    <script src="asset/activenavbar.js"></script>
+    <script src="asset/swiper-bundle.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        });
+    </script>
+</body>
+</html>
