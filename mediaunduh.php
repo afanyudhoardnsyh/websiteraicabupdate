d293b6d260ed4bc2fb6fa201e30ae6ee56eec3a1
afanyudhoardiansyah <121146698+afanyudhoardnsyh@users.noreply.github.com>
2024-04-27 22:01:05 +0700

    Add files via upload

---
+++ mediaunduh.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raicab | Unduh Berkas</title>
+    <link rel="stylesheet" href="css/styleunduh.css">
+    <!-- Fonts Awesome  Links For Icon -->
+    <script src="https://kit.fontawesome.com/383c3b5422.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <header>
+    <nav class="navbar">
+    <img src="image/OFC_WHITE Logo Raicab.webp" alt="Raimuna Cabang III Kota Depok">
+        <div class="logo" href="#">RAIMUNA CABANG III KOTA DEPOK</div>
+        <ul class="menu-links">
+        <span id="close-menu-btn" class="material-symbols-outlined">close</span>
+            <li><a href="index.php">BERANDA</a></li>
+            <li><a href="tentang.php">TENTANG</a></li>
+            <li><a href="mediaunduh.php" class="active">MEDIA UNDUH</a></li>
+            <li><a href="kegiatan.php">KEGIATAN</a></li>
+            <div><a href="./login/login.php" class="action_btn">MASUK</a></div>
+        </ul>
+        <!-- <a href="#" class="action_btn">MASUK</a> -->
+        <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
+    </nav>
+    </header>
+
+    <!-- media unduh section start -->
+    <section class="services" id="media-unduh">
+        <div class="max-width">
+            <h2 class="title">Silahkan unduh berkas raicab di bawah ini !</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                    <a href="pdf/SERTIFIKAT.pdf" download>
+                    <i class="fa-solid fa-circle-down"></i>
+                    </a>
+                        <div class="text">Surat Edaran</div>
+                        <p>Silahkan tekan icon di atas untuk mengunduh <span>Surat Edaran</span></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                    <a href="pdf/SERTIFIKAT.pdf" download>
+                        <i class="fa-solid fa-circle-down"></i>
+                    </a>
+                        <div class="text">Juklak & Juknis</div>
+                        <p>Silahkan tekan icon di atas untuk mengunduh <span>Juklak dan Juknis</span></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                    <a href="pdf/SERTIFIKAT.pdf" download>
+                        <i class="fa-solid fa-circle-down"></i>
+                    </a>
+                        <div class="text">GSM</div>
+                        <p>Silahkan tekan icon di atas untuk mengunduh <span>GSM </span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <div class="contact-content">
+                <div class="column left">
+                    <img src="image/OFC_Logo_Raicab_Polos.webp">
+                    <div class="text-1">RAIMUNA CABANG III</div>
+                    <div class="text-2">KOTA DEPOK</div>
+                    <div class="text-3">2024</div>
+                    <P> Ini adalah situs resmi Raimuna Cabang lll Tahun 2024. Konten yang tercantum di situs ini dimaksudkan untuk tujuan informasional, 
+                        bukan tujuan komersial. Setiap penjualan yang ditampilkan dimaksudkan sebagai tanda kemitraan dan selalu akan mengarahkan Anda 
+                        ke situs mitra kami.
+                    </P>
+                </div>
+                <div class="column right">
+                    <div class="text">Kontak Kami</div>
+                    <div class="alamat">
+                        <div class="text1">Alamat</div>
+                        <div class="text2">Jl. Boulevard Raya Grand Depok City Kalimulya, Kec. Cilodong, Kota Depok, Jawa Barat 16413</div>
+                    <div class="row">
+                        <div class="info-sosmed">
+                            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=afanyudhoardiansyah@gmail.com&su=Raicab%20III%202024&body=Halo Selamat Malam" target="_blank"><img class="email" style="margin:2px" src="image/gmail.webp" width="30" height="30" alt="Email"></a>
+                            <a href="https://www.instagram.com/dkc_kotadepok" target="_blank"><img  class="ig" style="margin:5px" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" width="25" height="25" alt="Instagram"></a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="container">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.7355482140233!2d106.82496667423598!3d-6.428011162858869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebf8cc5cf5bd%3A0x1cb0348aaa9190e1!2sGedung%20Pramuka%20Depok%20(Kwarcab%20Depok)!5e0!3m2!1sid!2sid!4v1709527268342!5m2!1sid!2sid" width="200" height="145" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <div class="row">
+            <div class="info-footer">
+                <a href="#" target="_blank"><img class="maskot" src="image/Maskot-Raicab.webp" alt="Maskot Raicab"></a>
+                <a href="#" target="_blank"><img class="logo" src="image/OFC_Logo_Raicab_Polos.webp" alt="Logo Raicab"></a>
+            </div>
+            <div class="text">Raimuna Cabang III</div>
+            <div class="text1">|</div> 
+            <div class="text2">2024 All rights reserved.</div> 
+        </div>
+    </footer>
+
+    <script src="asset/navbar.js"></script>
+    <script src="asset/activenavbar.js"></script>
+</body>
+</html>
