c24436cc7abb831980d3f09466a06e22339868d7
afanyudhoardiansyah <121146698+afanyudhoardnsyh@users.noreply.github.com>
2024-07-08 00:31:31 +0700

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
 $db_host                = 'localhost'; 
-$db_user                = 'root'; 
-$db_pass                = ''; 
-$db_name                = 'login'; 
+$db_user                = 'u864185394_raicab'; 
+$db_pass                = '=DLwVW/z2n8i'; 
+$db_name                = 'u864185394_raicab'; 
 
 $conn                = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
