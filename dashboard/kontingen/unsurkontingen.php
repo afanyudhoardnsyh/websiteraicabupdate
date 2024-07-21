8e56fc25b1c9e6ad879cfb4297cc65bd6c148328
afanyudhoardiansyah <121146698+afanyudhoardnsyh@users.noreply.github.com>
2024-07-21 16:20:39 +0700

    Update unsurkontingen.php

--- dashboard/kontingen/unsurkontingen.php
+++ dashboard/kontingen/unsurkontingen.php
@@ -220,7 +220,7 @@ $jumlah_peserta = mysqli_num_rows($data_peserta);
                             while($d = mysqli_fetch_array($data)){
                         ?>
                         <tr>
-                            <td><?php echo $no++; ?></td>
+                            <td style="text-align:center;><?php echo $no++; ?></td>
                             <td><?php echo $d['nama']; ?></td>
                             <td><?php echo $d['jenis_kelamin']; ?></td>
                             <td><?php echo $d['kwarran']; ?></td>
